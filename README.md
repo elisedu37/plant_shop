ef256a3033d3b0c9260f70efe9004488cc2a5cc3
elisedu37 <46353414+elisedu37@users.noreply.github.com>
2023-06-20 11:25:01 +0200

    Add first components

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # react_test
 
-[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/stackblitz-starters-nb37pm)
+Mon projet test avec StackBlitz
--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import React from "react";
-import "./style.css";
+import React from 'react';
+import './style.css';
+import Banner from './components/Banner';
+import Cart from './components/Cart';
 
 export default function App() {
   return (
     <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+      <Banner />
+      <Cart />
     </div>
   );
 }
