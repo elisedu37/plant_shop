ccfbce74af2aa16abc32a1d578dde7cc6a6cbb9d
elisedu37 <46353414+elisedu37@users.noreply.github.com>
2023-06-20 15:19:10 +0200

    fix logo

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Banner from './components/Banner';
 import Cart from './components/Cart';
 import ShoppingList from './components/ShoppingList';
 import Footer from './components/Footer';
-import './styles/Layout.css'
+import './styles/Layout.css';
 
 export default function App() {
   const savedCart = localStorage.getItem('cart');
@@ -18,7 +18,7 @@ export default function App() {
     <div>
       <Banner>
         <img
-          src="https://raw.githubusercontent.com/elisedu37/react_test/develop/src/assets/logo.png"
+          src="https://stackblitz.com/files/react-test-elise/github/elisedu37/react_test/develop/src/assets/logo.png"
           alt="La maison jungle"
           className="lmj-logo"
         />
