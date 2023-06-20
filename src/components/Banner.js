0c22dc4e869949d7b116cb84a9482d34dee345e1
elisedu37 <46353414+elisedu37@users.noreply.github.com>
2023-06-20 11:48:11 +0200

    1 binaries uploaded

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import '../styles/Banner.css';
 
 function Banner() {
-  return <h1>La maison jungle</h1>;
+  return (
+    <div className="lmj-banner">
+      <h1>La maison jungle</h1>
+    </div>
+  );
 }
 
 export default Banner;
