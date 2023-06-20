8e89f2e0366303de215fcfffcdd7156088614db0
elisedu37 <46353414+elisedu37@users.noreply.github.com>
2023-06-20 14:25:47 +0200

    Ajout de la liste des produits et styles

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './style.css';
 import Banner from './components/Banner';
 import Cart from './components/Cart';
+import ShoppingList from './components/ShoppingList';
 
 export default function App() {
   return (
     <div>
       <Banner />
       <Cart />
+      <ShoppingList />
     </div>
   );
 }
--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import '../styles/Banner.css';
-import logo from '../assets/logo.png';
 
 function Banner() {
   const title = 'La maison jungle';
   return (
     <div className="lmj-banner">
-      <img src="{logo}" alt="La maison jungle" className="lmj-logo" />
+      <img
+        src="https://raw.githubusercontent.com/elisedu37/react_test/develop/src/assets/logo.png"
+        alt="La maison jungle"
+        className="lmj-logo"
+      />
       <h1 className="lmj-title">{title}</h1>
     </div>
   );
