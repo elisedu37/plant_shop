f0f7c19467f8b0904ca78d65bbb5110c59ccbbac
elisedu37 <46353414+elisedu37@users.noreply.github.com>
2023-06-21 12:01:28 +0200

    ADD "charger plus"

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,15 +1,13 @@
-import React from 'react';
-// Hooks
-import { useState } from 'react';
-// Composants
+import React, { useState } from 'react';
 import { plantList } from '../datas/plantList';
 import PlantItem from './PlantItem';
 import Categories from './Categories';
 
-// Composant pour la liste de produits
 function ShoppingList({ cart, updateCart }) {
   const [activeCategory, setActiveCategory] = useState('');
   const [filter, setFilter] = useState('');
+  const [visibleProducts, setVisibleProducts] = useState(10);
+  const [isLoading, setIsLoading] = useState(false);
 
   const categories = plantList.reduce(
     (acc, elem) =>
@@ -37,6 +35,22 @@ function ShoppingList({ cart, updateCart }) {
     setFilter(filter.trim().toLowerCase());
   }
 
+  function handleLoadMore() {
+    setIsLoading(true);
+    setTimeout(() => {
+      setVisibleProducts((prevVisibleProducts) => prevVisibleProducts + 10);
+      setIsLoading(false);
+    }, 1000);
+  }
+
+  const filteredProducts = plantList.filter((r) =>
+    r.name.toLowerCase().includes(filter)
+  );
+
+  const displayedProducts = filteredProducts
+    .filter((product) => !activeCategory || activeCategory === product.category)
+    .slice(0, visibleProducts);
+
   return (
     <div className="lmj-shopping-list">
       <div className="filters">
@@ -49,40 +63,44 @@ function ShoppingList({ cart, updateCart }) {
       </div>
 
       <ul className="list-product">
-        {plantList
-          .filter((r) => r.name.toLowerCase().includes(filter))
-          .map(
-            ({
-              id,
-              cover,
-              name,
-              water,
-              light,
-              price,
-              category,
-              isBestSale,
-              isSpecialOffer,
-            }) =>
-              !activeCategory || activeCategory === category ? (
-                <div key={id} className="item">
-                  <PlantItem
-                    cover={cover}
-                    name={name}
-                    water={water}
-                    light={light}
-                    price={price}
-                    isBestSale={isBestSale}
-                    isSpecialOffer={isSpecialOffer}
-                  />
-                  <button onClick={() => addToCart(name, price)}>
-                    Ajouter
-                  </button>
-                </div>
-              ) : null
-          )}
-        {plantList.filter((r) => r.name.toLowerCase().includes(filter))
-          .length === 0 && <p>Aucun produit</p>}
+        {displayedProducts.map(
+          ({
+            id,
+            cover,
+            name,
+            water,
+            light,
+            price,
+            category,
+            isBestSale,
+            isSpecialOffer,
+          }) => (
+            <div key={id} className="item">
+              <PlantItem
+                cover={cover}
+                name={name}
+                water={water}
+                light={light}
+                price={price}
+                isBestSale={isBestSale}
+                isSpecialOffer={isSpecialOffer}
+              />
+              <button onClick={() => addToCart(name, price)}>Ajouter</button>
+            </div>
+          )
+        )}
+        {filteredProducts.length === 0 && <p>Aucun produit</p>}
       </ul>
+
+      {visibleProducts < filteredProducts.length && (
+        <div className="load-more">
+          {isLoading ? (
+            <p>Chargement...</p>
+          ) : (
+            <button onClick={handleLoadMore}>Afficher plus</button>
+          )}
+        </div>
+      )}
     </div>
   );
 }
--- src/datas/plantList.js
+++ src/datas/plantList.js
@@ -2,7 +2,102 @@ export const plantList = [
   {
     name: 'monstera',
     category: 'intérieur',
-    id: '1ed',
+    id: '1',
+    light: 2,
+    water: 2,
+    cover:
+      'https://images.pexels.com/photos/3125195/pexels-photo-3125195.jpeg?auto=compress&cs=tinysrgb&w=310&lazy=load',
+    price: 20,
+  },
+  {
+    name: 'ficus lyrata',
+    category: 'intérieur',
+    id: '2',
+    isBestSale: true,
+    light: 2,
+    water: 2,
+    cover:
+      'https://images.pexels.com/photos/7084310/pexels-photo-7084310.jpeg?auto=compress&cs=tinysrgb&h=220&lazy=load',
+    price: 30,
+  },
+  {
+    name: 'pothos argenté',
+    category: 'intérieur',
+    id: '3',
+    isSpecialOffer: true,
+    light: 1,
+    water: 2,
+    cover:
+      'https://images.pexels.com/photos/7663968/pexels-photo-7663968.jpeg?auto=compress&cs=tinysrgb&w=310&lazy=load',
+    price: 25,
+  },
+  {
+    name: 'yucca',
+    category: 'intérieur',
+    id: '4',
+    light: 3,
+    water: 1,
+    cover:
+      'https://images.pexels.com/photos/6207389/pexels-photo-6207389.jpeg?auto=compress&cs=tinysrgb&w=310&lazy=load',
+    price: 35,
+  },
+  {
+    name: 'olivier',
+    category: 'extérieur',
+    id: '5',
+    isSpecialOffer: true,
+    light: 3,
+    water: 1,
+    cover:
+      'https://images.pexels.com/photos/14763509/pexels-photo-14763509.jpeg?auto=compress&cs=tinysrgb&w=310&lazy=load',
+    price: 45,
+  },
+  {
+    name: 'géranium',
+    category: 'extérieur',
+    id: '6',
+    light: 3,
+    water: 2,
+    cover:
+      'https://images.pexels.com/photos/6769967/pexels-photo-6769967.jpeg?auto=compress&cs=tinysrgb&w=310&lazy=load',
+    price: 20,
+  },
+  {
+    name: 'Jardin de plante',
+    category: 'intérieur',
+    id: '7',
+    isSpecialOffer: true,
+    light: 1,
+    water: 2,
+    cover:
+      'https://images.pexels.com/photos/6913840/pexels-photo-6913840.jpeg?auto=compress&cs=tinysrgb&w=310&lazy=load',
+    price: 30,
+  },
+  {
+    name: 'aloe',
+    category: 'plante grasse',
+    id: '8',
+    isSpecialOffer: true,
+    light: 2,
+    water: 1,
+    cover:
+      'https://images.pexels.com/photos/1382394/pexels-photo-1382394.jpeg?auto=compress&cs=tinysrgb&h=220&lazy=load',
+    price: 15,
+  },
+  {
+    name: 'succulente',
+    category: 'plante grasse',
+    id: '9',
+    light: 3,
+    water: 1,
+    cover:
+      'https://images.pexels.com/photos/16328278/pexels-photo-16328278/free-photo-of-jardin-plantes-plantes-grasses-pots-de-fleurs.jpeg?auto=compress&cs=tinysrgb&w=310&lazy=load',
+    price: 12,
+  },
+  {
+    name: 'monstera',
+    category: 'intérieur',
+    id: '10',
     light: 2,
     water: 2,
     cover:
